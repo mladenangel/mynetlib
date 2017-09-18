47f10c4e0f4552637376fe921674b4c64f3fe2d5
mladen angel <mladen.angel@gmail.com>
2017-09-18 11:23:46 +0200

    Add files via upload

---
+++ pnda_aws_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+##############################################################################
+# delwin #####################################################################
+##############################################################################
+
+# Clone the Platform Salt
+
+git clone https://github.com/pndaproject/platform-salt.git
+cd platform-salt
+git checkout release/3.4.1
+
+cd ..
+
+# Clone the PNDA AWS Template latest release tag
+
+git clone git@github.com:pndaproject/pnda-aws-templates.git
+cd pnda-aws-templates
+git checkout release/3.4.1
+
+# Copy the sample pnda_env.yaml to the project after modifying as in the recipe.
+
+cp ../pnda_env.yaml pnda_env.yaml
+
+# Install Dependencies
+
+cd cli
+sudo pip install -r requirements.txt
+
+
